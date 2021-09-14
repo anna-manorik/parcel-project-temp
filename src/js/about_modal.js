97c4ab099d69f6c3f003b22d660f539b9ac9109b
Malanchenko Sergii <malan.sergii@gmail.com>
2021-09-14 21:44:58 +0300

    validation + bgimage milk were improve

--- src/js/about_modal.js
+++ src/js/about_modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[about_data-modal-open]'),
-    closeModalBtn: document.querySelector('[about_data-modal-close]'),
-    modal: document.querySelector('[about_data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
