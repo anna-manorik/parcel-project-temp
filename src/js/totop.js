7d69ca2048c59c220ac4b0a2e3d1b49822dc457d
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-09-15 18:20:00 +0300

    Create totop.js

---
+++ src/js/totop.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+      $(function() {
+      $(window).scroll(function() {
+      if($(this).scrollTop() != 0) {
+      $('.to-top').fadeIn();
+      } else {
+      $('.to-top').fadeOut();
+      }
+      });
+      $('.to-top').click(function() {
+      $('body,html').animate({scrollTop:0},800);
+      });
+      });
+    </script>
